891dcad3771f8b591c233cad087b11d52c71652a
kan.chen <kan.chen@xero.com>
2017-04-19 15:26:20 +0800

    react editor base

---
+++ App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const app = document.createElement('div');
+document.body.appendChild(app);
+ReactDOM.render(<div>Hello World</div>, app);
